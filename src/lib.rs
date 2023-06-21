4ac15b2fe2e596de2365b7c1ce876b7ffe187b5e
Milim <81323548+MilimTheTrueOne@users.noreply.github.com>
2023-06-21 14:46:49 +0200

    simd optmizations and use of nightly

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(portable_simd)]
+
 mod networks;
 mod training;
 
--- src/networks/neuron.rs
+++ src/networks/neuron.rs
@@ -1,3 +1,5 @@
+use std::simd::{f32x16, SimdFloat, f32x8, f32x4, f32x2};
+
 use rand::random;
 
 use super::*;
@@ -55,11 +57,49 @@ impl Neuron {
 
     #[inline]
     pub fn compute(&self, x: &[f32]) -> f32 {
-        let mut res = self.bias;
+        //let mut res =598s self.bias;
+        //let mut i = 0;
+        //while i < self.weights.len() {
+        //    res += self.weights[i] * x[i];
+        //    i += 1;
+        //}
+
+        let mut remaining_length = self.weights.len();
         let mut i = 0;
-        while i < self.weights.len() {
-            res += self.weights[i] * x[i];
-            i += 1;
+        let mut res = self.bias;
+        while remaining_length >= 16 {
+            let simd_weights = f32x16::from_slice(&self.weights[i..i+16]);
+            let simd_input = f32x16::from_slice(&x[i..i+16]);
+            res += (simd_input * simd_weights).reduce_sum();
+            i+=16;
+            remaining_length-=16;
+        }
+        while remaining_length >= 8 {
+            let simd_weights = f32x8::from_slice(&self.weights[i..i+8]);
+            let simd_input = f32x8::from_slice(&x[i..i+8]);
+            res += (simd_input * simd_weights).reduce_sum();
+            i+=8;
+            remaining_length-=8;
+        }
+        while remaining_length >= 4 {
+
+            let simd_weights = f32x4::from_slice(&self.weights[i..i+4]);
+            let simd_input = f32x4::from_slice(&x[i..i+4]);
+
+            res += (simd_input * simd_weights).reduce_sum();
+            i+=4;
+            remaining_length-=4;
+        }
+        while remaining_length >= 2 {
+            let simd_weights = f32x2::from_slice(&self.weights[i..i+2]);
+            let simd_input = f32x2::from_slice(&x[i..i+2]);
+            res += (simd_input * simd_weights).reduce_sum();
+            i+=16;
+            remaining_length-=16;
+        }
+        if remaining_length == 1 {
+            res += x[i] * self.weights[i];
+
         }
         match self.activation {
             ActivationFunction::Linear => res,
